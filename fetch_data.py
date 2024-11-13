4d769efc2741539b5ba27874d8bbf9b6cc3203d1
LCY000 <84628528+LCY000@users.noreply.github.com>
2024-11-13 17:23:55 +0800

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -7,7 +7,7 @@ tickers = ['AAPL', '006208.tw', '2330.tw']
 # 獲取今天的日期
 today = datetime.today().strftime('%Y-%m-%d')
 
-# 抓取數據，將 end 設定為今天
+# 抓取數據，將 end 設定為今天，先預設從2024年開始
 data = yf.download(tickers, start='2024-01-01', end=today)
 
 # 保存數據到 CSV 檔案
