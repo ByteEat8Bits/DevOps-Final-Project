73b79eea16955bf6667bc0a7fa6b55ae094269c3
LCY000 <84628528+LCY000@users.noreply.github.com>
2024-11-28 21:51:35 +0800

    抓台灣所有股票資訊 (#33)
    * 自動化抓所有台股股票CSV
    
    * 抓股票時不做資料清洗，網頁JS會做清洗

--- fetch_data.py
+++ fetch_data.py
@@ -1,8 +1,21 @@
 import yfinance as yf
 from datetime import datetime
+import requests  
+import json
+
+# 定義API的URL
+url = 'https://openapi.twse.com.tw/v1/exchangeReport/STOCK_DAY_ALL'  
+# 發送GET請求
+res = requests.get(url)  
+
+jsondata = json.loads(res.text)
+# print(jsondata)
+
+# 提取股票編號並加上 .TW
+stock_codes = [item["Code"] + ".TW" for item in jsondata]
 
 # 定義股票代碼
-tickers = ['AAPL', '006208.tw', '2330.tw']
+tickers = stock_codes
 
 # 獲取今天的日期
 today = datetime.today().strftime('%Y-%m-%d')
@@ -11,4 +24,4 @@ today = datetime.today().strftime('%Y-%m-%d')
 data = yf.download(tickers, start='2023-01-01', end=today)
 
 # 保存數據到 CSV 檔案
-data.to_csv('stock_data.csv')
+data.to_csv('stock_data_TW.csv')
