23f711aa335cebbf883330ac705d91e25ea90f3d
LCY000 <84628528+LCY000@users.noreply.github.com>
2024-11-30 00:02:42 +0800

    修正action抓取股票漏抓問題 v7 (#45)
    * 改用上版本的程式下載7個月資料，試試action執行效果

--- merge_data.py
+++ merge_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import numpy as np
 
 # 定義存放分批檔案的目錄
 directory = "./"  # 假設 CSV 文件存放在當前目錄
@@ -18,3 +19,17 @@ for file in csv_files:
 final_data = pd.concat(all_data, axis=1)  # 按列合併（不同股票的數據在列上）
 final_data.to_csv("stock_data_TW.csv")
 print("All batches merged and saved as merged_stock_data_TW.csv.")
+
+
+
+# 成功下載的股票數量
+file_path = "stock_data_TW.csv"
+data = pd.read_csv(file_path, header=None, low_memory=False)  # 不設定標題，完整讀取
+stock_names = data.iloc[1]  # 第 1 行是股票名稱
+
+# 使用 unique() 函數提取所有不同的股票名稱
+unique_stock_names = stock_names.unique()
+unique_stock_names = np.delete(unique_stock_names, np.where(unique_stock_names == 'Ticker'))
+# 計算下載股票的數量
+num_unique_stocks = len(unique_stock_names)
+print("\nNumber of stocks successfully downloaded:", num_unique_stocks)
